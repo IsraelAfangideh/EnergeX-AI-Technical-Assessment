6bbbde86748e10066249abb7da94a6313136c772
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2025-09-08 20:09:42 +0100

    finish user and post migrations

--- lumen-api/app/Models/Post.php
+++ lumen-api/app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -10,5 +11,10 @@ class Post extends Model
     protected $fillable = [
         'title', 'content',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
