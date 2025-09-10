e28459ccd935893e4859b853efc22efc55ce5b1d
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2025-09-10 17:11:05 +0100

    add navigation buttons for "New Post" and "Feed" to Header; format post data with author details in PostController

--- vue-frontend/src/components/Header.vue
+++ vue-frontend/src/components/Header.vue
@@ -1,17 +1,26 @@
 <script lang="ts" setup>
 import {Button} from "primevue";
 import {useUser} from "../composables/user-api";
+import {useRouter} from "vue-router";
 
 const {logout} = useUser();
+const router = useRouter()
 
+const goToCreatePost = () => {
+  router.push({name: 'create-post'})
+};
+const goToFeed = () => {
+  router.push({name: 'feed'})
+};
 </script>
 
 <template>
-  <div class="flex flex-row absolute top-10 right-10">
+  <div class="absolute top-10 right-10 flex gap-4">
+    <Button label="New Poast" severity="primary" @click="goToCreatePost"/>
+    <Button label="Feed" severity="success" @click="goToFeed"/>
     <Button label="Logout" severity="danger" @click="logout"/>
   </div>
 </template>
 
 <style scoped>
-
 </style>
